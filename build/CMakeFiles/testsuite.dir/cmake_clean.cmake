23d4f032fdbefc7e5d3dd4e4ae525828a811bb17
Dream0Eater <wakilul112@gmail.com>
2025-04-15 00:49:18 +0600

    msg

---
+++ build/CMakeFiles/testsuite.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testsuite.dir/tests/main_test.cpp.o"
+  "CMakeFiles/testsuite.dir/tests/main_test.cpp.o.d"
+  "testsuite"
+  "testsuite.pdb"
+  "testsuite[1]_tests.cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testsuite.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/testsuite[1]_tests.cmake
@@ -0,0 +1,5 @@
+add_test([=[LoggerTest.SingletonIdentity]=]  /home/akash/Desktop/Gtest_LabFinal/build/testsuite [==[--gtest_filter=LoggerTest.SingletonIdentity]==] --gtest_also_run_disabled_tests)
+set_tests_properties([=[LoggerTest.SingletonIdentity]=]  PROPERTIES WORKING_DIRECTORY /home/akash/Desktop/Gtest_LabFinal/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
+add_test([=[LoggerTest.LoggingOutput]=]  /home/akash/Desktop/Gtest_LabFinal/build/testsuite [==[--gtest_filter=LoggerTest.LoggingOutput]==] --gtest_also_run_disabled_tests)
+set_tests_properties([=[LoggerTest.LoggingOutput]=]  PROPERTIES WORKING_DIRECTORY /home/akash/Desktop/Gtest_LabFinal/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
+set(  testsuite_TESTS LoggerTest.SingletonIdentity LoggerTest.LoggingOutput)
